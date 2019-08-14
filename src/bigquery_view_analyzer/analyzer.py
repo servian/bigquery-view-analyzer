ebf7f16c4a362cd3870a89478b4c5d7ad03f7a11
Chris Tippett <c.tippett@gmail.com>
2019-08-14 19:31:49 +1000

    Update table patterns and tests

--- src/bigquery_view_analyzer/analyzer.py
+++ src/bigquery_view_analyzer/analyzer.py
@@ -7,10 +7,8 @@ from colorama import Fore, init
 from google.cloud import bigquery
 from google.cloud.bigquery import Table, AccessEntry, Dataset
 
-STANDARD_SQL_TABLE_PATTERN = r"`?(?:(?P<project>`?[^`]+?`?)(?:\.))?(?P<dataset>`?[^`]+?`?)\.(?P<table>`?[^`]+?`?)`?"
-LEGACY_SQL_TABLE_PATTERN = (
-    r"\[(?:(?P<project>.+?)(?:\:))?(?P<dataset>.+?)\.(?P<table>.+?)\]"
-)
+STANDARD_SQL_TABLE_PATTERN = r"(?:(?:FROM|JOIN)\s+?)?`?(?P<project>[-\w]+?)`?\.`?(?P<dataset>[-\w]+?)`?\.`?(?P<table>[-\w]+)`?"
+LEGACY_SQL_TABLE_PATTERN = r"(?:(?:FROM|JOIN)\s+?)?\[(?:(?P<project>[-\w]+?)(?:\:))?(?P<dataset>[-\w]+?)\.(?P<table>[-\w]+?)\]"
 
 logging.basicConfig()
 logging.captureWarnings(True)
