29d097926e8ffbe2900397f81699ed5ddee72ab6
Chris Tippett <c.tippett@gmail.com>
2019-08-14 17:12:02 +1000

    Update table pattern (standard SQL) to accommodate all valid ways of referencing a table. Fixes #4

--- tests/test_view_analyzer.py
+++ tests/test_view_analyzer.py
@@ -1,6 +1,38 @@
+import re
 import pytest
 
+from bigquery_view_analyzer.analyzer import (
+    STANDARD_SQL_TABLE_PATTERN,
+    LEGACY_SQL_TABLE_PATTERN,
+)
 
-def test_placeholder():
-    # Tests coming soon, I promise ;-)
-    assert True
+
+standard_table_references = [
+    "`project.dataset.table`",
+    "`project`.dataset.table",
+    "`project.dataset`.table",
+    "project.`dataset`.table",
+    "project.`dataset.table`",
+    "project.dataset.`table`",
+    "`project`.`dataset`.`table`",
+    "`dataset.table`",
+    "`dataset`.table",
+    "dataset.`table`",
+    "`dataset`.`table`",
+    "project.dataset.table",
+    "dataset.table",
+]
+
+legacy_table_references = ["[project.dataset.table]", "[dataset.table]"]
+
+
+@pytest.mark.parametrize("table_reference", standard_table_references)
+def test_standard_table_pattern(table_reference):
+    match = re.search(STANDARD_SQL_TABLE_PATTERN, table_reference, re.IGNORECASE)
+    assert match is not None
+
+
+@pytest.mark.parametrize("table_reference", legacy_table_references)
+def test_standard_table_pattern(table_reference):
+    match = re.search(LEGACY_SQL_TABLE_PATTERN, table_reference, re.IGNORECASE)
+    assert match is not None
